434d010432116275b10104f74afbd973d9f32d5d
Peter Milne <milnepe@gmail.com>
2023-06-22 21:31:19 +0100

    Make scantime optional with default

--- implementation/solutions/peripherals/gateway/gateway_adapters_no_security/do_discover_devices.py
+++ implementation/solutions/peripherals/gateway/gateway_adapters_no_security/do_discover_devices.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 """
-Discover devices test
+Discover devices test - optional scantime (default 2 seconds)
 
 Should pass:
 curl --request GET http://localhost/cgi-bin/gateway/gateway_adapters_no_security/do_discover_devices.py?scantime=3000
@@ -11,9 +11,6 @@ http://rock-3c/cgi-bin/gateway/gateway_adapters_no_security/do_discover_devices.
 Should fail:
 curl --header "Content-Type: application/json" --request PUT http://localhost/cgi-bin/gateway/gateway_adapters_no_security/do_discover_devices.py?scantime=3000
 Status-Line: HTTP/1.0 405 Method Not Allowed
-
-curl --request GET http://localhost/cgi-bin/gateway/gateway_adapters_no_security/do_discover_devices.py?xxxxxxxx=3000
-{"result": 8}
 """
 import os
 import json
@@ -22,7 +19,6 @@ import cgi
 
 sys.path.insert(0, '..')  # Aid apache to locate bluetooth package
 from bluetooth import bluetooth_gap
-from bluetooth import bluetooth_constants
 
 
 if 'REQUEST_METHOD' in os.environ:
@@ -36,13 +32,9 @@ if 'REQUEST_METHOD' in os.environ:
         print("Content-Type: application/json;charset=utf-8")
         print()
         querystring = cgi.FieldStorage()
-        if "scantime" not in querystring:
-            result['result'] = bluetooth_constants.RESULT_ERR_BAD_ARGS
-            print(json.JSONEncoder().encode(result))
-        else:
-            scantime = querystring.getfirst("scantime", "2000")
-            devices_discovered = bluetooth_gap.discover_devices(int(scantime))
-            devices_discovered_json = json.JSONEncoder().encode(devices_discovered)
-            print(devices_discovered_json)
+        scantime = querystring.getfirst("scantime", default="2000")
+        devices_discovered = bluetooth_gap.discover_devices(int(scantime))
+        devices_discovered_json = json.JSONEncoder().encode(devices_discovered)
+        print(devices_discovered_json)
 else:
     print("ERROR: Not called by HTTP")
