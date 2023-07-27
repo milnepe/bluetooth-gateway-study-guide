3f4bee5d07bce42e5023a99d2b4126f073cf3238
Peter Milne <milnepe@gmail.com>
2023-07-27 14:45:24 +0100

    Add default host to remote-client

--- implementation/solutions/peripherals/gateway/gateway_mqtt_no_security/remote-client.py
+++ implementation/solutions/peripherals/gateway/gateway_mqtt_no_security/remote-client.py
@@ -13,6 +13,11 @@ from bluetooth import bluetooth_utils
 from bluetooth import bluetooth_constants
 
 
+try:
+    host = sys.argv[1]
+except IndexError:
+    host = "localhost"
+
 temperature_sensors = ['{"bdaddr":"90:FD:9F:7B:7F:1C", "handle":"/org/bluez/hci0/dev_90_FD_9F_7B_7F_1C/service001b/char0020"}',
                        '{"bdaddr":"84:2E:14:31:C8:B0", "handle":"/org/bluez/hci0/dev_84_2E_14_31_C8_B0/service001f/char0022"}']
 
@@ -20,7 +25,7 @@ temperature_sensors = ['{"bdaddr":"90:FD:9F:7B:7F:1C", "handle":"/org/bluez/hci0
 def send_command():
     while True:
         for t in temperature_sensors:
-            publish.single("test/gateway/in/read_characteristic", t, hostname="localhost")
+            publish.single("test/gateway/in/read_characteristic", t, hostname=host)
 
         sleep(2)
 
