28bc6349b1763fd8d08857fca53a67f231544cfa
milnepe <milnepe@gmail.com>
2023-08-08 22:06:35 +0100

    Lint

--- implementation/solutions/peripherals/gateway/bluetooth/bluetooth_exceptions.py
+++ implementation/solutions/peripherals/gateway/bluetooth/bluetooth_exceptions.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python
+"""Custom exceptions"""
+
 
 class StateError(RuntimeError):
+    """Device state error"""
+
     def __init__(self, arg):
         self.args = str(arg)
 
 
 class UnsupportedError(RuntimeError):
+    """Device unsupported error"""
+
     def __init__(self, arg):
         self.args = str(arg)
