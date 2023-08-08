43b5fb2db75b3d24b7e44fe2091f5b2ac17f5107
milnepe <milnepe@gmail.com>
2023-08-08 22:00:12 +0100

    Lint imports

--- implementation/solutions/peripherals/gateway/gateway_mqtt_no_security/mqtt_client.py
+++ implementation/solutions/peripherals/gateway/gateway_mqtt_no_security/mqtt_client.py
@@ -50,12 +50,15 @@ Subscribe to outbound messages
 mosquitto_sub -h localhost -t "test/gateway/out/#"
 """
 
-import paho.mqtt.client as mqtt
 import logging
 import json
 import sys
+from threading import Thread
+import dbus
+import dbus.mainloop.glib
+import paho.mqtt.client as mqtt
 
-sys.path.insert(0, '..')  # Aid location of bluetooth package
+sys.path.insert(0, "..")  # Aid location of bluetooth package
 from bluetooth import bluetooth_constants
 
 from commands import CmdDiscoverDevices
@@ -67,21 +70,6 @@ from commands import CmdNotifications
 from bt_controller import BtController, Notifier
 from invoker import Invoker
 
-
-from bluetooth import bluetooth_utils
-from bluetooth import bluetooth_general
-from bluetooth import bluetooth_exceptions
-from bluetooth import bluetooth_constants
-import dbus
-import dbus.mainloop.glib
-from dbus import ByteArray
-from threading import Thread
-from threading import local
-from sys import stdin, stdout
-import time
-import codecs
-from operator import itemgetter, attrgetter
-
 try:
     # from gi.repository import GObject
     import gi.repository.GLib
@@ -89,12 +77,15 @@ except ImportError:
     # import gobject as GObject
     print("gi.repository.GLib import not found")
 
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 
 # must set main loop before acquiring SystemBus object
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 bus = dbus.SystemBus()
-manager = dbus.Interface(bus.get_object(bluetooth_constants.BLUEZ_SERVICE_NAME, "/"), bluetooth_constants.DBUS_OM_IFACE)
+manager = dbus.Interface(
+    bus.get_object(bluetooth_constants.BLUEZ_SERVICE_NAME, "/"),
+    bluetooth_constants.DBUS_OM_IFACE,
+)
 
 bt_controller = BtController()
 invoker = Invoker()
@@ -109,60 +100,85 @@ def mainloop_task():
 def on_discover_devices(mosq, obj, msg):
     """Callback mapping TOPIC_ROOT + "/in/discover_devices" topic to CmdDiscoverDevices"""
     payload = json.loads(msg.payload)
-    invoker.set_command(CmdDiscoverDevices(bt_controller, payload['scantime']))
-    logging.info("Discover devices: %s, %s", msg.topic, msg.payload.decode('utf-8'))
+    invoker.set_command(CmdDiscoverDevices(bt_controller, payload["scantime"]))
+    logging.info("Discover devices: %s, %s", msg.topic, msg.payload.decode("utf-8"))
 
 
 def on_connect_device(mosq, obj, msg):
     """Callback mapping TOPIC_ROOT + "/in/connect_device" topic to CmdConnectDevice"""
     payload = json.loads(msg.payload)
-    invoker.set_command(CmdConnectDevice(bt_controller, payload['bdaddr']))
-    logging.info("Connect device: %s, %s", msg.topic, msg.payload.decode('utf-8'))
+    invoker.set_command(CmdConnectDevice(bt_controller, payload["bdaddr"]))
+    logging.info("Connect device: %s, %s", msg.topic, msg.payload.decode("utf-8"))
 
 
 def on_write_characteristic(mosq, obj, msg):
     """Callback mapping TOPIC_ROOT + "/in/write_characteristic" topic to CmdWriteCharacteristic"""
     payload = json.loads(msg.payload)
-    invoker.set_command(CmdWriteCharacteristic(bt_controller, payload['bdaddr'], payload['handle'], payload['value']))
-    logging.info("Write Characteristic: %s, %s", msg.topic, msg.payload.decode('utf-8'))
+    invoker.set_command(
+        CmdWriteCharacteristic(
+            bt_controller, payload["bdaddr"], payload["handle"], payload["value"]
+        )
+    )
+    logging.info("Write Characteristic: %s, %s", msg.topic, msg.payload.decode("utf-8"))
 
 
 def on_discover_services(mosq, obj, msg):
     """Callback mapping TOPIC_ROOT + "/in/discover_services" topic to CmdDiscoverServices"""
     payload = json.loads(msg.payload)
-    invoker.set_command(CmdDiscoverServices(bt_controller, payload['bdaddr']))
-    logging.info("Discover Services: %s, %s", msg.topic, msg.payload.decode('utf-8'))
+    invoker.set_command(CmdDiscoverServices(bt_controller, payload["bdaddr"]))
+    logging.info("Discover Services: %s, %s", msg.topic, msg.payload.decode("utf-8"))
 
 
 def on_read_characteristic(mosq, obj, msg):
     """Callback mapping TOPIC_ROOT + "/in/read_characteristic" topic to CmdReadCharacteristic"""
     payload = json.loads(msg.payload)
-    invoker.set_command(CmdReadCharacteristic(bt_controller, payload['bdaddr'], payload['handle']))
-    logging.info("Read Characteristic: %s, %s", msg.topic, msg.payload.decode('utf-8'))
+    invoker.set_command(
+        CmdReadCharacteristic(bt_controller, payload["bdaddr"], payload["handle"])
+    )
+    logging.info("Read Characteristic: %s, %s", msg.topic, msg.payload.decode("utf-8"))
 
 
 def on_notifications(mosq, obj, msg):
     """Callback mapping TOPIC_ROOT + "/in/notifications" topic to CmdNotifications"""
     payload = json.loads(msg.payload)
-    invoker.set_command(CmdNotifications(Notifier(payload['bdaddr'], payload['handle'], payload['command'])))
-    logging.info("Notifications: %s, %s", msg.topic, msg.payload.decode('utf-8'))
+    invoker.set_command(
+        CmdNotifications(
+            Notifier(payload["bdaddr"], payload["handle"], payload["command"])
+        )
+    )
+    logging.info("Notifications: %s, %s", msg.topic, msg.payload.decode("utf-8"))
 
 
 def on_message(mosq, obj, msg):
     """Callback mapping all other TOPIC_ROOT messages - no ops"""
-    logging.info("Unexpected message: %s, %s", msg.topic, msg.payload.decode('utf-8'))
+    logging.info("Unexpected message: %s, %s", msg.topic, msg.payload.decode("utf-8"))
 
 
 def main() -> None:
+    """Run client"""
 
     mqttc = mqtt.Client()
 
-    mqttc.message_callback_add(bluetooth_constants.TOPIC_ROOT + "/in/discover_devices", on_discover_devices)
-    mqttc.message_callback_add(bluetooth_constants.TOPIC_ROOT + "/in/connect_device", on_connect_device)
-    mqttc.message_callback_add(bluetooth_constants.TOPIC_ROOT + "/in/write_characteristic", on_write_characteristic)
-    mqttc.message_callback_add(bluetooth_constants.TOPIC_ROOT + "/in/discover_services", on_discover_services)
-    mqttc.message_callback_add(bluetooth_constants.TOPIC_ROOT + "/in/read_characteristic", on_read_characteristic)
-    mqttc.message_callback_add(bluetooth_constants.TOPIC_ROOT + "/in/notifications", on_notifications)
+    mqttc.message_callback_add(
+        bluetooth_constants.TOPIC_ROOT + "/in/discover_devices", on_discover_devices
+    )
+    mqttc.message_callback_add(
+        bluetooth_constants.TOPIC_ROOT + "/in/connect_device", on_connect_device
+    )
+    mqttc.message_callback_add(
+        bluetooth_constants.TOPIC_ROOT + "/in/write_characteristic",
+        on_write_characteristic,
+    )
+    mqttc.message_callback_add(
+        bluetooth_constants.TOPIC_ROOT + "/in/discover_services", on_discover_services
+    )
+    mqttc.message_callback_add(
+        bluetooth_constants.TOPIC_ROOT + "/in/read_characteristic",
+        on_read_characteristic,
+    )
+    mqttc.message_callback_add(
+        bluetooth_constants.TOPIC_ROOT + "/in/notifications", on_notifications
+    )
 
     mqttc.on_message = on_message
     mqttc.connect(bluetooth_constants.BROKER, 1883, 60)
