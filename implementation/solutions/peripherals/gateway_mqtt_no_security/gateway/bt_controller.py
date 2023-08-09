0d16a0ed1cc6831aa55fe4f08022e6c167f5b86e
milnepe <milnepe@gmail.com>
2023-08-09 11:20:18 +0100

    Lint

--- implementation/solutions/peripherals/gateway_mqtt_no_security/gateway/bt_controller.py
+++ implementation/solutions/peripherals/gateway_mqtt_no_security/gateway/bt_controller.py
@@ -1,27 +1,33 @@
+"""Bluetooth LE device controller"""
+
 import logging
 import sys
 import json
-import dbus
-from threading import Thread
 from dataclasses import dataclass
-
-sys.path.insert(0, '..')  # Aid location of bluetooth package
-from bluetooth import bluetooth_gap
-from bluetooth import bluetooth_gatt
-from bluetooth import bluetooth_exceptions
-from bluetooth import bluetooth_utils
-from bluetooth import bluetooth_constants
-from bluetooth import bluetooth_general
-
+import dbus
 import paho.mqtt.publish as publish
+
+sys.path.insert(0, "..")  # Aid location of bluetooth package
+from bluetooth_api import bluetooth_gap
+from bluetooth_api import bluetooth_gatt
+from bluetooth_api import bluetooth_exceptions
+from bluetooth_api import bluetooth_utils
+from bluetooth_api import bluetooth_constants
+from bluetooth_api import bluetooth_general
 
 
 class BtController:
+    """Bluetooth LE Controller"""
 
     @staticmethod
     def publisher(topic: str, result: dict):
+        """Publishing helper"""
         json_result = json.JSONEncoder().encode(result)
-        publish.single(bluetooth_constants.TOPIC_ROOT + "/out/" + topic, json_result, hostname=bluetooth_constants.BROKER)
+        publish.single(
+            bluetooth_constants.TOPIC_ROOT + "/out/" + topic,
+            json_result,
+            hostname=bluetooth_constants.BROKER,
+        )
 
     def discover_devices(self, scantime: str) -> None:
         """Discover devices in controller with optional timeout"""
@@ -33,69 +39,77 @@ class BtController:
     def connect_device(self, bdaddr: str) -> None:
         """Connect device using address"""
         result = {}
-        rc = bluetooth_gap.connect(bdaddr)
-        result['result'] = rc
-        result['bdaddr'] = bdaddr
-        result['cmd'] = "connect_device"
+        result_code = bluetooth_gap.connect(bdaddr)
+        result["result"] = result_code
+        result["bdaddr"] = bdaddr
+        result["cmd"] = "connect_device"
         logging.info(json.JSONEncoder().encode(result))
         BtController.publisher("connection", result)  # Publish result
 
     def write_characteristic(self, bdaddr: str, handle: str, value: str) -> None:
         """Write a value to the characteristic using device address"""
         result = {}
-        result['bdaddr'] = bdaddr
-        result['handle'] = handle
-        result['cmd'] = "write_characteristic"
-        result['value'] = value
+        result["bdaddr"] = bdaddr
+        result["handle"] = handle
+        result["cmd"] = "write_characteristic"
+        result["value"] = value
         try:
-            rc = bluetooth_gatt.write_characteristic(bdaddr, handle, value)
-            result['result'] = rc
-        except bluetooth_exceptions.StateError as e:
-            result['result'] = e.args[0]
+            result_code = bluetooth_gatt.write_characteristic(bdaddr, handle, value)
+            result["result"] = result_code
+        except bluetooth_exceptions.StateError as error:
+            result["result"] = error.args[0]
         logging.info(json.JSONEncoder().encode(result))
         BtController.publisher("sensor", result)  # Publish result
 
     def discover_services(self, bdaddr: str) -> None:
         """Discover services using device address"""
         result = {}
-        result['cmd'] = "discover_services"
+        result["cmd"] = "discover_services"
         try:
             services_discovered = bluetooth_gatt.get_services(bdaddr)
 
             for service in services_discovered:
                 # rename BlueZ-specific parameter name to the more abstract 'handle'
-                service['handle'] = service.pop('path')
-                characteristics_discovered = bluetooth_gatt.get_characteristics(bdaddr, service['handle'])
+                service["handle"] = service.pop("path")
+                characteristics_discovered = bluetooth_gatt.get_characteristics(
+                    bdaddr, service["handle"]
+                )
 
-                service['characteristics'] = characteristics_discovered
+                service["characteristics"] = characteristics_discovered
                 for characteristic in characteristics_discovered:
-                    characteristic['handle'] = characteristic.pop('path')
-                    characteristic['service_handle'] = characteristic.pop('service_path')
-                    descriptors_discovered = bluetooth_gatt.get_descriptors(bdaddr, characteristic['handle'])
+                    characteristic["handle"] = characteristic.pop("path")
+                    characteristic["service_handle"] = characteristic.pop(
+                        "service_path"
+                    )
+                    descriptors_discovered = bluetooth_gatt.get_descriptors(
+                        bdaddr, characteristic["handle"]
+                    )
 
-                    characteristic['descriptors'] = descriptors_discovered
+                    characteristic["descriptors"] = descriptors_discovered
                     for descriptor in descriptors_discovered:
-                        descriptor['handle'] = descriptor.pop('path')
-                        descriptor['characteristic_handle'] = descriptor.pop('characteristic_path')
+                        descriptor["handle"] = descriptor.pop("path")
+                        descriptor["characteristic_handle"] = descriptor.pop(
+                            "characteristic_path"
+                        )
 
             result = json.JSONEncoder().encode(services_discovered)
-        except bluetooth_exceptions.StateError as e:
-            result['result'] = e.args[0]
+        except bluetooth_exceptions.StateError as error:
+            result["result"] = error.args[0]
         logging.info(json.JSONEncoder().encode(result))
         BtController.publisher("services", result)  # Publish result
 
     def read_characteristic(self, bdaddr: str, handle: str):
         """Read a characteristic using device address and handle"""
         result = {}
-        result['bdaddr'] = bdaddr
-        result['handle'] = handle
-        result['cmd'] = "read_characteristic"
+        result["bdaddr"] = bdaddr
+        result["handle"] = handle
+        result["cmd"] = "read_characteristic"
         try:
             value = bluetooth_gatt.read_characteristic(bdaddr, handle)
-            result['value'] = bluetooth_utils.byteArrayToHexString(value)
-            result['result'] = 0
-        except bluetooth_exceptions.StateError as e:
-            result['result'] = e.args[0]
+            result["value"] = bluetooth_utils.byteArrayToHexString(value)
+            result["result"] = 0
+        except bluetooth_exceptions.StateError as error:
+            result["result"] = error.args[0]
         logging.info(json.JSONEncoder().encode(result))
         BtController.publisher("sensor", result)
 
@@ -104,17 +118,19 @@ class BtController:
 class Notifier:
     """Notification class allowing each device to have its own event handler
     [Todo: fix stop notifications]"""
+
     bdaddr: str
     handle: str
     command: str
     notifications_callback = None
 
     def properties_changed(self, interface, changed, invalidated, path):
+        """Properties changed"""
         if interface != "org.bluez.GattCharacteristic1":
             return
 
         value = []
-        value = changed.get('Value')
+        value = changed.get("Value")
         if not value:
             return
         if self.notifications_callback:
@@ -124,53 +140,81 @@ class Notifier:
         mainloop.quit()
 
     def start_notifications(self, characteristic_iface):
+        """Start notifications helper"""
         bus = dbus.SystemBus()
-        bus.add_signal_receiver(self.properties_changed, bus_name=bluetooth_constants.BLUEZ_SERVICE_NAME,
-                                dbus_interface=bluetooth_constants.DBUS_PROPERTIES,
-                                signal_name="PropertiesChanged",
-                                path_keyword="path")
+        bus.add_signal_receiver(
+            self.properties_changed,
+            bus_name=bluetooth_constants.BLUEZ_SERVICE_NAME,
+            dbus_interface=bluetooth_constants.DBUS_PROPERTIES,
+            signal_name="PropertiesChanged",
+            path_keyword="path",
+        )
 
         bus.add_signal_receiver(self.stop_handler, "StopNotifications")
 
         characteristic_iface.StartNotify()
 
     def enable_notifications(self, bdaddr, characteristic_path, callback):
+        """Enable notification with callback"""
         self.notifications_callback = callback
         bus = dbus.SystemBus()
         device_proxy = bluetooth_general.getDeviceProxy(bus, bdaddr)
         if not device_proxy:
-            raise bluetooth_exceptions.StateError(bluetooth_constants.RESULT_ERR_NOT_CONNECTED)
+            raise bluetooth_exceptions.StateError(
+                bluetooth_constants.RESULT_ERR_NOT_CONNECTED
+            )
         device_path = device_proxy.object_path
 
         if not bluetooth_general.is_connected(bus, device_path):
-            raise bluetooth_exceptions.StateError(bluetooth_constants.RESULT_ERR_NOT_CONNECTED)
+            raise bluetooth_exceptions.StateError(
+                bluetooth_constants.RESULT_ERR_NOT_CONNECTED
+            )
 
         if not device_proxy.ServicesResolved:
-            raise bluetooth_exceptions.StateError(bluetooth_constants.RESULT_ERR_SERVICES_NOT_RESOLVED)
+            raise bluetooth_exceptions.StateError(
+                bluetooth_constants.RESULT_ERR_SERVICES_NOT_RESOLVED
+            )
 
-        characteristic_object = bus.get_object(bluetooth_constants.BLUEZ_SERVICE_NAME, characteristic_path)
-        characteristic_iface = dbus.Interface(characteristic_object, bluetooth_constants.GATT_CHARACTERISTIC_INTERFACE)
-        properties_iface = dbus.Interface(characteristic_object, bluetooth_constants.DBUS_PROPERTIES)
-        characteristic_properties = properties_iface.Get(bluetooth_constants.GATT_CHARACTERISTIC_INTERFACE, "Flags")
+        characteristic_object = bus.get_object(
+            bluetooth_constants.BLUEZ_SERVICE_NAME, characteristic_path
+        )
+        characteristic_iface = dbus.Interface(
+            characteristic_object, bluetooth_constants.GATT_CHARACTERISTIC_INTERFACE
+        )
+        properties_iface = dbus.Interface(
+            characteristic_object, bluetooth_constants.DBUS_PROPERTIES
+        )
+        characteristic_properties = properties_iface.Get(
+            bluetooth_constants.GATT_CHARACTERISTIC_INTERFACE, "Flags"
+        )
 
-        if 'notify' not in characteristic_properties and 'indicate' not in characteristic_properties:
-            raise bluetooth_exceptions.UnsupportedError(bluetooth_constants.RESULT_ERR_NOT_SUPPORTED)
+        if (
+            "notify" not in characteristic_properties
+            and "indicate" not in characteristic_properties
+        ):
+            raise bluetooth_exceptions.UnsupportedError(
+                bluetooth_constants.RESULT_ERR_NOT_SUPPORTED
+            )
 
         # Returns dbus.Boolean!
-        notifying = properties_iface.Get(bluetooth_constants.GATT_CHARACTERISTIC_INTERFACE, "Notifying")
+        notifying = properties_iface.Get(
+            bluetooth_constants.GATT_CHARACTERISTIC_INTERFACE, "Notifying"
+        )
         notifying = bool(notifying)
         if notifying is True:
-            raise bluetooth_exceptions.StateError(bluetooth_constants.RESULT_ERR_WRONG_STATE)
+            raise bluetooth_exceptions.StateError(
+                bluetooth_constants.RESULT_ERR_WRONG_STATE
+            )
         self.start_notifications(characteristic_iface)
 
     def notification_received(self, path, value):
         """Notifications callback"""
         result = {}
-        bdaddr_from_path = path.replace('_', ':')[20:37]  # Hack!
-        result['bdaddr'] = self.bdaddr
-        result['handle'] = path
-        result['cmd'] = "notification_received"
-        result['value'] = bluetooth_utils.byteArrayToHexString(value)
+        bdaddr_from_path = path.replace("_", ":")[20:37]  # Hack!
+        result["bdaddr"] = self.bdaddr
+        result["handle"] = path
+        result["cmd"] = "notification_received"
+        result["value"] = bluetooth_utils.byteArrayToHexString(value)
         if bdaddr_from_path == self.bdaddr:
             logging.info(json.JSONEncoder().encode(result))
 
@@ -180,23 +224,27 @@ class Notifier:
         if self.command == 0:
             try:
                 bluetooth_gatt.disable_notifications(bdaddr, handle)
-                result['result'] = bluetooth_constants.RESULT_OK
-            except bluetooth_exceptions.StateError as e:
-                result['result'] = e.args[0]
-            except bluetooth_exceptions.UnsupportedError as e:
-                result['result'] = e.args[0]
-            except dbus.exceptions.DBusException as e:
-                result['result'] = e.args[0]
-            logging.info("Notifications disabled: %s", json.JSONEncoder().encode(result))
+                result["result"] = bluetooth_constants.RESULT_OK
+            except bluetooth_exceptions.StateError as error:
+                result["result"] = error.args[0]
+            except bluetooth_exceptions.UnsupportedError as error:
+                result["result"] = error.args[0]
+            except dbus.exceptions.DBusException as error:
+                result["result"] = error.args[0]
+            logging.info(
+                "Notifications disabled: %s", json.JSONEncoder().encode(result)
+            )
         elif self.command == 1:
             try:
-                self.enable_notifications(self.bdaddr, self.handle, self.notification_received)
-                result['result'] = bluetooth_constants.RESULT_OK
-            except bluetooth_exceptions.StateError as e:
-                result['result'] = e.args[0]
-            except bluetooth_exceptions.UnsupportedError as e:
-                result['result'] = e.args[0]
+                self.enable_notifications(
+                    self.bdaddr, self.handle, self.notification_received
+                )
+                result["result"] = bluetooth_constants.RESULT_OK
+            except bluetooth_exceptions.StateError as error:
+                result["result"] = error.args[0]
+            except bluetooth_exceptions.UnsupportedError as error:
+                result["result"] = error.args[0]
             logging.info("Notifications enabled: %s", json.JSONEncoder().encode(result))
         else:
-            result['result'] = bluetooth_constants.RESULT_ERR_BAD_ARGS
+            result["result"] = bluetooth_constants.RESULT_ERR_BAD_ARGS
             logging.info("Bad command: %s", json.JSONEncoder().encode(result))
